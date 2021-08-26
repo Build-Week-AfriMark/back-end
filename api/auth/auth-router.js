e64b95fd43d78621726731780c8b03609d1e48cc
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-26 12:26:55 -0700

    checks if username is unique upon register

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -9,25 +9,39 @@ const validateCreds = (req, res, next) => {
 let user = req.body
 if (!user.username || !user.password) {
   res.status(400).json({
-    message: "username and password required"
+    message: "username and/or password required"
   })
 } else {
   next()
 }
 }
 
+// const validateUsername = (req, res, next) => {
+//   User.getUsername(req.body.username)
+
+//   .then(data => {
+//     if (data) {
+//       next({status:400, message: "username taken"})
+//     } else {
+//       next()
+//     }
+//   })
+//   .catch(next)
+// }
+
 const validateUsername = (req, res, next) => {
   User.getUsername(req.body.username)
   .then(data => {
     if (data) {
-      next({status:400, message: "username taken"})
+      next(
+        res.status(400).json({message: `username ${req.body.username} taken`})
+      )
     } else {
       next()
     }
   })
   .catch(next)
 }
-
 
 
 router.post('/register', validateUsername, validateCreds,  (req, res, next) => {
